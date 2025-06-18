906cc5fff69fbf86ab6d29fe898bfedcc79bc5ff
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-18 09:31:51 -0300

    Add client src/index.tsx entry point

---
+++ client/src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { ContentProvider } from './context/ContentContext';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ContentProvider>
+          <App />
+        </ContentProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
